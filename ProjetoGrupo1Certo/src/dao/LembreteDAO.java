e73f2ebf871d1355f9e230ffb5901647ace22072
Flora Marone <you@example.com>
2021-07-09 20:01:15 -0300

    commit

--- ProjetoGrupo1Certo/src/dao/LembreteDAO.java
+++ ProjetoGrupo1Certo/src/dao/LembreteDAO.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
  */
 package dao;
 
@@ -17,6 +15,7 @@ import java.util.ArrayList;
  *
  * @author flora
  */
+
 public class LembreteDAO {
     
     public void cadastrarLembrete(LembreteVO lVO) throws SQLException{
